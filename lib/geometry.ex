2a1ce06c977fdea09c3ddc049c2283c60d8666a2
sandeeperanti <sandeep@gmx.ru>
2023-05-17 16:09:25 +0530

    repo

---
+++ lib/geometry.ex
@@ -0,0 +1,18 @@
+defmodule Geometry do
+  @moduledoc """
+  Documentation for `Geometry`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Geometry.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
