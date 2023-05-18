04522f33445a44a72d7af47d94384cc011508dd2
sandeeperanti <sandeep@gmx.ru>
2023-05-18 11:01:37 +0530

    add pparams

--- lib/geometry.ex
+++ lib/geometry.ex
@@ -1,18 +1,7 @@
 defmodule Geometry do
-  @moduledoc """
-  Documentation for `Geometry`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Geometry.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def polygon do
+    @type: triangle
+    @type: square
+    @type: circle
   end
 end
---
+++ lib/geometry/parameters.ex
@@ -0,0 +1,5 @@
+defmodule Geometry.Parameters do
+  def triangle do
+    IO.gets("side,angle,angle")
+  end
+end
