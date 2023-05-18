719709d66179cec527888063790dc2326e33246a
sandeeperanti <sandeep@gmx.ru>
2023-05-18 11:04:05 +0530

    add pparams

--- lib/geometry.ex
+++ lib/geometry.ex
@@ -1,7 +1,11 @@
 defmodule Geometry do
+  def read_integer(prompt) do
+    IO.gets(prompt) |> String.trim() |> String.to_integer()
+  end
+
   def polygon do
-    @type: triangle
-    @type: square
-    @type: circle
+    # @type: triangle
+    # @type: square
+    # @type: circle
   end
 end
--- lib/geometry/parameters.ex
+++ lib/geometry/parameters.ex
@@ -1,5 +1,34 @@
 defmodule Geometry.Parameters do
-  def triangle do
-    IO.gets("side,angle,angle")
+  import Geometry, only: [read_integer: 1]
+
+  def circle_area do
+    radius = read_integer("enter radius of the circle: ")
+    area = 3.14 * radius * radius
+    IO.puts("Circle Area is #{area}")
+  end
+
+  def triangle_area do
+    base = read_integer("enter base of the triangle: ")
+
+    height = read_integer("enter height of the triangle: ")
+
+    area = base * height / 2
+    IO.puts("Triangle Area is #{area}")
+  end
+
+  def rectangle_area do
+    length = read_integer("enter length of the rectangle: ")
+
+    breadth = read_integer("enter breadth of the rectangle: ")
+
+    area = length * breadth
+    IO.puts("Triangle Area is #{area}")
+  end
+
+  def square_area do
+    side = read_integer("enter side of the square: ")
+
+    area = side * side
+    IO.puts("Triangle Area is #{area}")
   end
 end
